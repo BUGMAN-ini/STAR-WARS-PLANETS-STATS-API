999d758175034facaeb4130c066e85ed2f0dd75b
BUGMAN-ini <bshaverda@gmail.com>
2024-11-06 20:55:59 +0400

    added some operators

--- STAR WARS PLANETS STATS/Program.cs
+++ STAR WARS PLANETS STATS/Program.cs
@@ -1,18 +1,118 @@
 ﻿using STAR_WARS_PLANETS_STATS.ApiDataAccess;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
-const string ExpectedBaseAddress = "https://swapi.dev/";
-const string ExpectedRequestUri = "api/planets";
-var fullUrl = "https://swapi.dev/api/planets";
+try
+{
+    await new StarWarsPlanetStatsApp(
+        new ApiDataReader(),
+        new MockStarWarsApiDataReader()).Run();
+}
+catch (Exception ex)
+{
 
-
-IApiDataReader apiDataReader = new ApiDataReader();
-var json = await apiDataReader.Read(fullUrl);
-
-
+    Console.WriteLine(ex.Message);
+}
 
 Console.ReadKey();
+
+public class StarWarsPlanetStatsApp
+{
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondapidata;
+    public StarWarsPlanetStatsApp(IApiDataReader apiDataReader, IApiDataReader secondapidata)
+    {
+        _apiDataReader = apiDataReader;
+        _secondapidata = secondapidata;
+    }
+
+
+
+
+    public async Task Run()
+    {
+        string? json = null;
+        try
+        {
+            json = await _apiDataReader.Read("https://swapi.dev/", "api/planets");
+
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        if (json is null)
+        {
+            json = await _secondapidata.Read("https://swapi.dev/", "api/planets");
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+
+        var planets = ToPlanets(root);
+    }
+
+    private IEnumerable<Planet> ToPlanets(Root? root)
+    {
+        if (root is null)
+        {
+            throw new ArgumentNullException(nameof(root));
+        }
+        
+        var planets = new List<Planet>();
+
+        foreach (var planetDto in root.results)
+        {
+            Planet planet = (Planet)planetDto;
+            planets.Add(planet);
+        }
+
+        return planets;
+    }
+
+    public readonly record struct Planet
+    {
+        public string Name { get; }
+        public int Diameter { get; }
+        public int? SurfaceWater { get; }
+        public int? Population { get; }
+
+        public Planet(
+            string name,
+            int diameter,
+            int surface,
+            int population)
+        {
+            if (name is null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            Name = name;
+            Diameter = diameter;
+            SurfaceWater = surface;
+            Population = population;
+        }
+
+        public static explicit operator Planet(Result planetDto)
+        {
+            var name = planetDto.name;
+            var diameter = int.Parse(planetDto.diameter);
+            int? population = null;
+            if (int.TryParse(
+                planetDto.population,out int populationParsed))
+            {
+                population = populationParsed;
+            }
+            int? SurfaceTemp = null;
+            if (int.TryParse(
+                planetDto.population, out int surfacetemperature))
+            {
+                SurfaceTemp = surfacetemperature;
+            }
+        }
+    }
+}
+
+
+
 
 
 
